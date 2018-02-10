6f451cdd16d05b99fd109f3a02e4414ee11f8103
BecaWang <w121093@hotmail.com.tw>
2018-02-10 16:14:20 +0800

    springboot + maven

---
+++ src/example/controller/HelloBootController.java
@@ -0,0 +1,13 @@
+package example.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloBootController {
+	
+	@RequestMapping("/")
+	public String helloPage(){
+		return "Hello! Spring Boot and maven";
+	}
+}
