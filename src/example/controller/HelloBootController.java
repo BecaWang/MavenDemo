254d24bec1db4c925e2751b5979424da4bc9b95d
BecaWang <w121093@hotmail.com.tw>
2018-02-10 16:20:05 +0800

    test url

--- src/example/controller/HelloBootController.java
+++ src/example/controller/HelloBootController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloBootController {
 	
+	//http://localhost:8080/
+	
 	@RequestMapping("/")
 	public String helloPage(){
 		return "Hello! Spring Boot and maven";
